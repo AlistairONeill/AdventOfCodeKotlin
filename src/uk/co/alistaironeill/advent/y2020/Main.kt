9db34e921bb65a2f07779493c45a97888b8fd6fe
alistaironeill <me@alistaironeill.co.uk>
2020-12-01 09:31:33 +0000

    Initial commit

---
+++ src/uk/co/alistaironeill/advent/y2020/Main.kt
@@ -0,0 +1,7 @@
+package uk.co.alistaironeill.advent.y2020
+
+import uk.co.alistaironeill.advent.y2020.d01.ExpenseReport
+
+fun main() {
+
+}
---
+++ src/uk/co/alistaironeill/advent/y2020/extensions/List.kt
@@ -0,0 +1,3 @@
+package uk.co.alistaironeill.advent.y2020.extensions
+
+fun <E> List<E>.subList(startIndex: Int) = subList(startIndex, size)
